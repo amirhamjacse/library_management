6cad5bb076593e9508f62b515f894e7cc67e7711
amirhamjacse <amirhamza824@gmail.com>
2025-02-05 16:34:05 +0600

    url added

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BookListCreateAPIView, BookDetailAPIView
+from .views import (
+    BookListCreateAPIView, BookDetailAPIView,
+    BorrowBookAPIView, ReturnBookAPIView, BorrowedBooksAPIView
+)
 
 
 urlpatterns = [
     path("list-create/", BookListCreateAPIView.as_view(), name="book-list-create"),
     path("details/<int:book_id>/", BookDetailAPIView.as_view(), name="book-detail"),
+    path('borrow/<int:book_id>/', BorrowBookAPIView.as_view(), name='borrow-book'),
+    path('return/<int:book_id>/', ReturnBookAPIView.as_view(), name='return-book'),
+    path('borrowed/', BorrowedBooksAPIView.as_view(), name='borrowed-books'),
 ]
